21f7ac34954e7e3ec19b84d61151483a5650d801
zolizoli <zoltan.varju@gmail.com>
2018-08-24 13:30:43 +0200

    refactor image downloader

--- src/get_mapillary_imgs.py
+++ src/get_mapillary_imgs.py
@@ -9,24 +9,26 @@ out_path = 'data/imgs'
 
 fs = [f for f in listdir(in_path) if isfile(join(in_path, f))]
 imgs = [f for f in listdir(out_path) if isfile(join(out_path, f))]
+imgs = set([f.split('|')[0] for f in imgs])
 for f in fs:
-    with open(join(in_path, f), 'r') as inputfile:
-        d = json.load(inputfile)
-        features = d['features']
-        if len(features) > 0:
-            i = 0
-            for feature in features:
-                img_id = feature['properties']['key']
-                base = 'https://www.mapillary.com/map/im/'
-                url = base + img_id
-                html = urlopen(url).read()
-                soup = BeautifulSoup(html)
-                links = soup.find_all('meta')
-                links = [l['content'] for l in links if l.has_attr('content')]
-                links = set([l for l in links if l.endswith('jpg')])
-                for link in links:
-                    filename = f.split('.')[0] + '|' + str(i) + '.jpg'
-                    if filename not in imgs:
+    if f.split('.')[0] not in imgs:
+        print(f)
+        with open(join(in_path, f), 'r') as inputfile:
+            d = json.load(inputfile)
+            features = d['features']
+            if len(features) > 0:
+                i = 0
+                for feature in features:
+                    img_id = feature['properties']['key']
+                    base = 'https://www.mapillary.com/map/im/'
+                    url = base + img_id
+                    html = urlopen(url).read()
+                    soup = BeautifulSoup(html)
+                    links = soup.find_all('meta')
+                    links = [l['content'] for l in links if l.has_attr('content')]
+                    links = set([l for l in links if l.endswith('jpg')])
+                    for link in links:
+                        filename = f.split('.')[0] + '|' + str(i) + '.jpg'
                         response = urlopen(link).read()
                         with open(join(out_path, filename), 'wb') as outfile:
                             outfile.write(response)
